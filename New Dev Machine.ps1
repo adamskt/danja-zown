071dad17ad71985abeb1a989e96436a6d8352425
Kelly Adams <kelly@intellitect.com>
2018-05-14 11:17:49 -0700

    Adding script for provisioning a new machine

---
+++ New Dev Machine.ps1
@@ -0,0 +1,24 @@
+﻿# Run in administrative Powershell
+choco feature enable -n allowGlobalConfirmation
+choco install 7zip
+# choco install SharpKeys  # Broken since CodePlex went into Archive Mode
+choco install Everything
+choco install gitextensions
+choco install sysinternals --svc
+choco install openssh
+choco install speccy
+choco install cmder
+choco install microsoft-teams
+choco install gpg4win
+choco install ag
+choco install filezilla
+choco install share
+choco install notepad2
+choco install nodejs
+choco install chocolatey.extension
+choco install gpg4win 
+choco install notepad2 
+choco install nmap  
+choco install sharex  
+
+choco feature disable -n allowGlobalConfirmation
