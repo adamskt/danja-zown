8039f5a1fbe0b1151b14c69013a6db9ad3c2bf87
Kelly Adams <kelly@intellitect.com>
2019-09-26 15:03:24 -0700

    Updated with VS2019 16.3 change to location of Microsoft.VisualStudio.DevShell and fixed colors in windowsterminal

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,23 +1,27 @@
 # PSReadline Options
 Set-PSReadlineOption -EditMode Vi -HistoryNoDuplicates
 Set-PSReadlineOption -ViModeIndicator Cursor
+Set-PSReadlineOption -Colors @{
+  "Operator" = "$([char]0x1b)[32m"
+  "Parameter" = "$([char]0x1b)[92m"
+}
 
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
-Set-PSReadlineKeyHandler -Key Ctrl+Tab -Function PossibleCompletions
+Set-PSReadlineKeyHandler -Chord UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Chord DownArrow -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Chord Ctrl+spacebar -Function PossibleCompletions
 
 #$env:NAME = $env:COMPUTERNAME # Stupid fix because $PSVersionTable.Platform returns "Win32NT" not "Windows"
 
 # oh-my-posh Options
-Set-Theme Agnoster
+Set-Theme Paradox
 
 # PSColor options
-
+Import-Module PSColor
 $global:PSColor.File.Hidden.Color = 'Gray'
 
 # Borrowed from https://gist.github.com/jtucker/6886367fb58d5404032507576b43433f
 $installPath = &"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -version 16.0 -property installationpath
-Import-Module (Join-Path $installPath "Common7\Tools\vsdevshell\Microsoft.VisualStudio.DevShell.dll")
+Import-Module (Join-Path $installPath "Common7\Tools\Microsoft.VisualStudio.DevShell.dll")
 Enter-VsDevShell -VsInstallPath $installPath -SkipAutomaticLocation
 
 # Stolen from https://github.com/scottmuc/poshfiles/blob/master/Microsoft.PowerShell_profile.ps1
